19f89b432758ebc6f77af301db377adaf5c048d8
Jesus Laiz <zheileman@users.noreply.github.com>
2024-10-16 09:46:29 +0100

    Fix rubocop offences

--- spec/lib/health_spec.rb
+++ spec/lib/health_spec.rb
@@ -8,30 +8,30 @@ describe Health do
   describe "status" do
     context "when all status checks return { 'status': 'UP' }" do
       it "is UP" do
-        health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
-        health.add_check(name: 'serviceB', get_response: -> { { "status" => "UP" } })
+        health.add_check(name: "serviceA", get_response: -> { { "status" => "UP" } })
+        health.add_check(name: "serviceB", get_response: -> { { "status" => "UP" } })
 
         expect(health.status).to eq({
           status: "UP",
           components: {
             "serviceA" => { status: "UP" },
             "serviceB" => { status: "UP" },
-          }
+          },
         })
       end
     end
 
     context "when any of the status checks return { 'status': 'DOWN' }" do
       it "is DOWN" do
-        health.add_check(name: 'serviceA', get_response: -> { { "status" => "DOWN" } })
-        health.add_check(name: 'serviceB', get_response: -> { { "status" => "UP" } })
+        health.add_check(name: "serviceA", get_response: -> { { "status" => "DOWN" } })
+        health.add_check(name: "serviceB", get_response: -> { { "status" => "UP" } })
 
         expect(health.status).to eq({
           status: "DOWN",
           components: {
             "serviceA" => { status: "DOWN" },
             "serviceB" => { status: "UP" },
-          }
+          },
         })
       end
     end
@@ -40,16 +40,16 @@ describe Health do
       context "when the response matches that value" do
         it "is UP" do
           health.add_check(
-            name: 'serviceA',
+            name: "serviceA",
             get_response: -> { { "custom" => "value" } },
-            check_response: ->(response) { response["custom"] == "value" }
+            check_response: ->(response) { response["custom"] == "value" },
           )
 
           expect(health.status).to eq({
             status: "UP",
             components: {
               "serviceA" => { status: "UP" },
-            }
+            },
           })
         end
       end
@@ -57,16 +57,16 @@ describe Health do
       context "when the response does not match that value" do
         it "is DOWN" do
           health.add_check(
-            name: 'serviceA',
+            name: "serviceA",
             get_response: -> { { "custom" => "not_value" } },
-            check_response: { key: "custom", value: "value" }
+            check_response: { key: "custom", value: "value" },
           )
 
           expect(health.status).to eq({
             status: "DOWN",
             components: {
               "serviceA" => { status: "DOWN" },
-            }
+            },
           })
         end
       end
@@ -76,33 +76,33 @@ describe Health do
       context "when the response matches that value" do
         it "is UP" do
           health.add_check(
-            name: 'serviceA',
+            name: "serviceA",
             get_response: -> { "pong" },
-            check_response: ->(response) { response == "pong" }
+            check_response: ->(response) { response == "pong" },
           )
 
           expect(health.status).to eq({
             status: "UP",
             components: {
               "serviceA" => { status: "UP" },
-            }
+            },
           })
         end
       end
 
       context "when the response does not match that value" do
-        it "is UP" do
+        it "is DOWN" do
           health.add_check(
-            name: 'serviceA',
+            name: "serviceA",
             get_response: -> { "not_pong" },
-            check_response: { value: "pong" }
+            check_response: { value: "pong" },
           )
 
           expect(health.status).to eq({
             status: "DOWN",
             components: {
               "serviceA" => { status: "DOWN" },
-            }
+            },
           })
         end
       end
@@ -110,32 +110,32 @@ describe Health do
 
     describe "when any of the status checks fail" do
       it "is DOWN" do
-        health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
-        health.add_check(name: 'serviceB', get_response: -> { raise "FAILED!" })
+        health.add_check(name: "serviceA", get_response: -> { { "status" => "UP" } })
+        health.add_check(name: "serviceB", get_response: -> { raise "FAILED!" })
 
         expect(health.status).to eq({
           status: "DOWN",
           components: {
             "serviceA" => { status: "UP" },
             "serviceB" => { status: "DOWN" },
-          }
+          },
         })
       end
 
       context "when the check passes after retrying once" do
         it "is UP" do
-          momentarily_down = double('momentarily_down')
+          momentarily_down = instance_double(Proc)
           allow(momentarily_down).to receive(:call).and_return({ "status" => "DOWN" }, { "status" => "UP" })
 
-          health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
-          health.add_check(name: 'serviceB', get_response: momentarily_down)
+          health.add_check(name: "serviceA", get_response: -> { { "status" => "UP" } })
+          health.add_check(name: "serviceB", get_response: momentarily_down)
 
           expect(health.status).to eq({
             status: "UP",
             components: {
               "serviceA" => { status: "UP" },
               "serviceB" => { status: "UP" },
-            }
+            },
           })
         end
       end
@@ -144,13 +144,13 @@ describe Health do
         it "is DOWN" do
           allow(Timeout).to receive(:timeout).and_raise(Timeout::Error)
 
-          health.add_check(name: 'serviceA', get_response: -> { { "status" => "UP" } })
+          health.add_check(name: "serviceA", get_response: -> { { "status" => "UP" } })
 
           expect(health.status).to eq({
             status: "DOWN",
             components: {
               "serviceA" => { status: "DOWN" },
-            }
+            },
           })
         end
       end
