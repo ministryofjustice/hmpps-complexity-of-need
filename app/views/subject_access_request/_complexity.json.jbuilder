1e6d04a82638b6e1420f9dfae0f8523770911fee
Jesus L <zheileman@users.noreply.github.com>
2026-03-09 16:38:42 +0000

    MO-1912 Remove offenderNo from SAR response (#167)
    * MO-1912 Remove offenderNo from SAR response
    
    Ticket: https://dsdmoj.atlassian.net/browse/MO-1912
    
    This aligns the response with the current template.
    
    * Linting

--- app/views/subject_access_request/_complexity.json.jbuilder
+++ app/views/subject_access_request/_complexity.json.jbuilder
@@ -1,6 +1,5 @@
 # frozen_string_literal: true
 
-json.offenderNo complexity.offender_no
 json.extract! complexity, :level
 json.createdTimeStamp complexity.created_at
 json.updatedTimeStamp complexity.updated_at
--- spec/requests/v1/subject_access_request_spec.rb
+++ spec/requests/v1/subject_access_request_spec.rb
@@ -37,11 +37,10 @@ RSpec.describe "Subject access request", type: :request do
     end
 
     shared_examples "returns expected complexities" do
-      it "returns expected complexity records whether active or not" do # rubocop:disable RSpec/ExampleLength
+      it "returns expected complexity records whether active or not" do
         expect(response_json).to eq json_object({
           content: expected_complexities.map do |c|
             {
-              offenderNo: c.offender_no,
               level: c.level,
               notes: c.notes,
               createdTimeStamp: c.created_at,
