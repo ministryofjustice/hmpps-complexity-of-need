fbd4cb4a20fd6de889bf117e109fca80d7a3f5e5
Jesus L <zheileman@users.noreply.github.com>
2026-05-20 15:07:27 +0100

    Disable app insights track_exception (#193)
    Mostly backported from the implementation on MPC, because CNL has the same issue with application insights hijacking and messing with sentry exceptions.
    
    https://github.com/ministryofjustice/offender-management-allocation-manager/pull/2816

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,15 +1,60 @@
 # frozen_string_literal: true
 
-Sentry.init do |config|
-  # 'DSN' and 'environment' are automatically pulled in from
-  # environment variables SENTRY_DSN and SENTRY_CURRENT_ENV
-  # so no need to configure them here
+sentry_dsn = Rails.configuration.sentry_dsn
 
-  config.breadcrumbs_logger = [:active_support_logger]
-  config.release = ENV["BUILD_NUMBER"]
-  config.enable_metrics = false
+if sentry_dsn.present?
+  require "active_support/parameter_filter"
+  param_filter = ActiveSupport::ParameterFilter.new(Rails.application.config.filter_parameters)
 
-  config.before_send = lambda do |event, hint|
-    hint[:exception].full_message.match?(/ApplicationInsights::TelemetryClient/) ? nil : event
+  Sentry.init do |config|
+    config.dsn = sentry_dsn
+    config.release = ENV["BUILD_NUMBER"]
+    config.enable_metrics = false
+    config.rails.report_rescued_exceptions = false
+
+    if Rails.env.development?
+      config.background_worker_threads = 0
+      config.sdk_logger = Sentry::Logger.new($stdout).tap { it.level = Logger::DEBUG }
+    end
+
+    config.excluded_exceptions += %w[
+      JWT::ExpiredSignature
+      JWT::VerificationError
+      ActiveRecord::RecordNotFound
+      ActionController::RoutingError
+      ActionController::UnknownFormat
+      ActionController::InvalidAuthenticityToken
+      ActionController::BadRequest
+      Faraday::ConnectionFailed
+      Faraday::TimeoutError
+    ]
+
+    config.before_send = lambda do |event, hint|
+      begin
+        # Sanitize extra data
+        if event.extra
+          event.extra = param_filter.filter(event.extra)
+        end
+
+        # Sanitize user data
+        if event.user
+          event.user = param_filter.filter(event.user)
+        end
+
+        # Sanitize context data
+        if event.contexts
+          event.contexts = param_filter.filter(event.contexts)
+        end
+      rescue StandardError => e
+        Rails.logger.warn(
+          "event=sentry_sanitization_error|original_exception_class=#{hint[:exception]&.class}|#{e.message}",
+        )
+      end
+
+      # Return the sanitized event object
+      event
+    end
   end
+else
+  Rails.logger.warn "[WARN] Sentry is not configured (SENTRY_DSN)"
 end
