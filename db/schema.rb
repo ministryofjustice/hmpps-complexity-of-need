92088b0b8f4795fd22e33b9abd8e841d94777285
Jesus L <zheileman@users.noreply.github.com>
2026-06-18 15:20:17 +0100

    Regenerate schema using current rails version (#198)
    It did change some bits but it is safe and the standard with Rails 8.x

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[6.1].define(version: 2022_07_25_144655) do
-
+ActiveRecord::Schema[8.1].define(version: 2022_07_25_144655) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pg_catalog.plpgsql"
   enable_extension "pgcrypto"
-  enable_extension "plpgsql"
 
   create_table "complexities", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
-    t.string "offender_no", null: false
-    t.string "level", null: false
-    t.string "source_system", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "source_user"
-    t.string "notes"
     t.boolean "active", default: true
+    t.datetime "created_at", null: false
+    t.string "level", null: false
+    t.string "notes"
+    t.string "offender_no", null: false
+    t.string "source_system", null: false
+    t.string "source_user"
+    t.datetime "updated_at", null: false
     t.index ["offender_no"], name: "index_complexities_on_offender_no"
   end
-
 end
